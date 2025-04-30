edefdc4e090460f6348c09f27e223282af19dad3
migmadee <migmademasherpa079@gmail.com>
2025-04-30 02:34:51 -0700

    foot

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AppRoutes from "./components/AppRoutes"
+import Footer from "./components/Foot"
 import NavBar from "./components/NavBar"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
     <NavBar/>
       <AppRoutes/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Foot/index.tsx
@@ -0,0 +1,44 @@
+import { Box, Text, Heading, Flex, Image } from '@chakra-ui/react'
+import dragon1 from '../../assets/Image/dragon1.jpeg'
+import dragon2 from '../../assets/Image/dragon2.jpeg'
+
+const Footer = () => {
+  return (
+    <Box
+      as="footer"
+      bg="gray.900"
+      color="white"
+      py={8}
+      position="fixed"
+      bottom={0}
+      width="100%"
+    >
+      <Flex
+        maxW="1000px"
+        mx="auto"
+        justify="space-between"
+        align="center"
+        px={4}
+      >
+  
+  <Image src={dragon1} alt="Left Logo" boxSize="150px" borderRadius="full" objectFit="cover" />
+
+     
+        <Box textAlign="center">
+          <Heading fontWeight="bold" size="md" mb={4}>Contact Us</Heading>
+          <Text>Email: groupTwo@gmail.com</Text>
+          <Text>Phone: +975-12345678</Text>
+          <Text>Address: Tsirang, Bhutan</Text>
+          <Text>Location: Damphu Municipal Hall</Text>
+          <Text></Text>
+          <Heading fontSize="20px" fontWeight="bold" mt={3}>BHUTAN</Heading>
+        </Box>
+
+       
+        <Image src={dragon2} alt="Right Logo" boxSize="150px" borderRadius="full" objectFit="cover" />
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
