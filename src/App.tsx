0f2d9798137d5499e7d8d1b5686c4647f9b16ba8
sonamza <sonamzangmo1002@gmail.com>
2025-04-30 01:21:10 -0700

    Head

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import AppRoutes from "./components/AppRoutes"
+import Header from "./components/Head"
 import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
+    <Header/>
     <NavBar/>
       <AppRoutes/>
     </>
