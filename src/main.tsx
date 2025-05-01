7eee4ba475607040a76362d722a1964c1cf6d0fa
migmadee <migmademasherpa079@gmail.com>
2025-05-01 00:11:52 -0700

    contactform

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
+
+import 'react-toastify/dist/ReactToastify.css';
+
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
